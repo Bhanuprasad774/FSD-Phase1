0cd278ad8fbb1be91d7c0a1d181ab3f7b3477a0b
bhanuprasad774 <bhanuprasad774@gmail.com>
2022-06-19 15:33:14 +0530

    initial commit

---
+++ src/corejavaproject/Basics.java
@@ -0,0 +1,15 @@
+package corejavaproject;
+
+public class Basics {
+	company name = Bhanu;
+	speed = 100;
+
+}
+class car{
+	String company;
+	int speed;
+	void getspeed() {
+		System.out.println("company+carspeed");
+		
+	}
+}
